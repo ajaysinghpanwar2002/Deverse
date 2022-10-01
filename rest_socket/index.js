aa20f4c4e700e84720e1ebe18214e37c308b5bfb
Ajay <ajaysingh07032003@gmal.com>
2022-10-01 12:44:06 +0530

    updated package.json for github page hosting

--- rest_socket/index.js
+++ rest_socket/index.js
@@ -2,17 +2,17 @@ const express = require ("express");
 
 const app = express();
 
-app.set("view engine", "ejs");
+app.set("view engine", "html");
 
 app.set(express.urlencoded({extended:false}));
 
 app.set(express.json());
 
 app.get('/',(req,res)=>{
-    res.send("index");
+    res.render("index");
 
 });
-
+ 
 app.listen(3001,()=>{
     console.log("server is running on port 3001");
 })
